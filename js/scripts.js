c73a24f3229d71521f88c05066c9c1577436ddb2
raeman21 <mandelar94@gmail.com>
2018-07-04 13:55:46 +0300

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $("h1").click(function() {
+    alert("This is a header.");
+  });
+
+  $("p").click(function() {
+    alert("This is a paragraph.");
+  });
+
+  $("img").click(function() {
+    alert("This is an image.");
+  });
+
+    $("h2").click(function() {
+      alert("This is heading 2");
+    });
+  });
